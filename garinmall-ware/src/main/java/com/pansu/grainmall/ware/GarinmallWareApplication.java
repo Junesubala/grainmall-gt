6384d47efc031a59207b5343094618f63dc01139
pansu <2973424208@qq.com>
2022-04-17 17:02:40 +0800

    项目初始结构创建

---
+++ garinmall-ware/src/main/java/com/pansu/grainmall/ware/GarinmallWareApplication.java
@@ -0,0 +1,13 @@
+package com.pansu.grainmall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarinmallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarinmallWareApplication.class, args);
+    }
+
+}
